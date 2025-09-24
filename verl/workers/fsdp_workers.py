7b4d56af4a13aeca91b44f625cedda27edc29a75
Arun Lokanatha <alokana@amazon.com>
2025-09-23 17:57:26 -0700

    update fsdp_workers.py

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -997,8 +997,8 @@ class ActorRolloutRefWorker(Worker, DistProfilerExtension):
         #     local_path=local_path, hdfs_path=hdfs_path, global_step=global_step,s3_base_path=self.config.s3_base_path, ckpt_namespace=self.config.ckpt_namespace, max_ckpt_to_keep=max_ckpt_to_keep
         # )
 
-        logger.log("printing config parameters")
-        logger.log("configs are {self.config}")
+        logger.log(logging.INFO,"printing config parameters")
+        logger.log(logging.INFO,"configs are {self.config}")
       
         self.checkpoint_manager.save_checkpoint(
             local_path=local_path, hdfs_path=hdfs_path, global_step=global_step,s3_base_path="s3://sagemaker-hyperpod-eks-ak-bucket-535002850097-us-west-1/checkpoints", ckpt_namespace="testnsray14", max_ckpt_to_keep=max_ckpt_to_keep
