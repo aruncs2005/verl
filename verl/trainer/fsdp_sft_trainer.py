ffe60f6164a935dd3b817e5718428a9291145e66
Arun Lokanatha <alokana@amazon.com>
2025-10-09 21:53:02 -0700

    fix

--- verl/trainer/fsdp_sft_trainer.py
+++ verl/trainer/fsdp_sft_trainer.py
@@ -579,7 +579,7 @@ class FSDPSFTTrainer:
                 f"step {self.resume_global_step}",
             )
         except Exception as e:
-            print(f"[Rank {self.device_mesh.get_rank()}]load_checkpoint failed:{str(e)}",
+            log_with_rank(f"[Rank {self.device_mesh.get_rank()}]load_checkpoint failed:{str(e)}",
                   logger=logger, rank=self.device_mesh.get_rank())
 
     def fit(self):
